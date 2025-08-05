9dd9cd0e68249460cba1b2ba8292f647d958b026
Zen Vendor <zen.vendor@mixbox.pl>
2025-08-05 20:14:30 +0200

    model design

--- model.go
+++ model.go
@@ -7,11 +7,44 @@ import (
 	"time"
 )
 
+type Wallet struct {
+	gorm.Model
+	DebitLimit    float32
+	CreditLimit   float32
+	CurrentAmount float32
+	Name          string
+	Description   string
+}
+
+type Recurrent struct {
+	gorm.Model
+	StartDate   time.Time
+	EndDate     time.Time
+	Amount      float32
+	Name        string
+	Description string
+	PayByDay    uint8
+}
+
+type Category struct {
+	gorm.Model
+	ParentID    uint
+	Name        string
+	Description string
+}
+
 type Transaction struct {
 	gorm.Model
 	TransactionDate time.Time
+	Amount          float32
+	WalletID        uint
+	RecurrentID     uint
+	TransactionID   uint
+	CategoryID      uint
 	Description     string
-	Amount          float64
+	IsOutgoing      bool
+	IsTransaction   bool
+	IsRecurrent     bool
 }
 
 func prepare() (db *gorm.DB, ctx context.Context, err error) {
@@ -20,6 +53,9 @@ func prepare() (db *gorm.DB, ctx context.Context, err error) {
 		return nil, nil, err
 	}
 	ctx = context.Background()
+	db.AutoMigrate(&Wallet{})
+	db.AutoMigrate(&Recurrent{})
+	db.AutoMigrate(&Category{})
 	db.AutoMigrate(&Transaction{})
 
 	return db, ctx, nil
